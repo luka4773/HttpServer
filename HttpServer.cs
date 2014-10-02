9bf5a94cfbdaa2d195f6a3dcd6908852e93575f8
luka4773 <lukas.cech12@gmail.com>
2014-10-02 15:51:23 +0200

    Starting from scratch, took some basic work on colleagues and worked on it a little. Hopefully looks better and a bit cleaner.

---
+++ HttpServer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HttpServer
+{
+    internal class HttpServer
+    {
+
+        public static readonly int DefaultPort = 8080;
+
+      
+       public  HttpServer()
+        {
+            IPAddress localAddress = IPAddress.Parse("127.0.0.1");
+            TcpListener serverSocket = new TcpListener(localAddress, DefaultPort);
+            serverSocket.Start();
+            Console.WriteLine("Server is up.");
+            Socket sock = serverSocket.AcceptSocket();
+
+            while (true)
+            {
+                TcpClient tcp = serverSocket.AcceptTcpClient();
+                Console.WriteLine("Client connected");
+                Stream server = tcp.GetStream();
+                StreamReader sr = new StreamReader(server);
+                StreamWriter sw = new StreamWriter(server);
+                string request = sr.ReadLine();
+                sw.Write("Hey, the time of your request was: " +DateTime.Now);
+              
+                sw.AutoFlush = true;
+                tcp.Close();
+
+            }
+        }
+     
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HttpServer http = new HttpServer();
+            Console.WriteLine("Server started.");
+            
+        }
+    }
+}
