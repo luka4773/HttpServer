5a9063d17b5b4573ffb421c0cc45409ba292b89e
luka4773 <lukas.cech12@gmail.com>
2014-10-02 16:05:06 +0200

    Next part of the assignment, needs polishing - will do later.. There's a stupid switch between dynamic and static response of the server, has to be manually changed.. but meh it's ok for my purposes

--- HttpServer.cs
+++ HttpServer.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -14,7 +15,7 @@ namespace HttpServer
     {
 
         public static readonly int DefaultPort = 8080;
-
+        private bool staticDynamic = false;
       
        public  HttpServer()
         {
@@ -24,7 +25,7 @@ namespace HttpServer
             Console.WriteLine("Server is up.");
             Socket sock = serverSocket.AcceptSocket();
 
-            while (true)
+            while (staticDynamic)
             {
                 TcpClient tcp = serverSocket.AcceptTcpClient();
                 Console.WriteLine("Client connected, lets go.");
@@ -38,6 +39,25 @@ namespace HttpServer
                 tcp.Close();
 
             }
+
+           while (true)
+           {
+               TcpClient tcp = serverSocket.AcceptTcpClient();
+               Console.WriteLine("Dynamic client connected, lets go.");
+               Stream server = tcp.GetStream();
+               StreamReader sr = new StreamReader(server);
+               StreamWriter sw = new StreamWriter(server);
+               string s = sr.ReadLine();
+               if (s != null)
+               {
+                   s = s.Split('/')[1];
+                   s = s.Remove(s.Length - 5);
+               }
+               sw.Write("Hey, the file you have requested was: " + s + " at " + DateTime.Now);
+
+               sw.AutoFlush = true;
+               tcp.Close();
+           }
         }
      
     }
