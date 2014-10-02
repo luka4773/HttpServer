6ff5f859a0f08cd51d41054f52e56b02ab2a69b0
luka4773 <lukas.cech12@gmail.com>
2014-10-02 17:28:54 +0200

    Added more dumb choices how to choose between what kind of server is running(static/dynamic/openfile etc...).
    For some reason catching exceptions in 1c only throws blank pages, not 404 not found as it should. Not gonna look into that now.

--- HttpServer.cs
+++ HttpServer.cs
@@ -13,9 +13,12 @@ namespace HttpServer
 {
     internal class HttpServer
     {
-
+        private static readonly string RootCatalog = @"C:\Users\Lukas\Documents";
         public static readonly int DefaultPort = 8080;
-        private bool staticDynamic = false;
+        private bool Static = false;
+        private bool Dynamic = false;
+        private bool openFile = true;
+        
       
        public  HttpServer()
         {
@@ -25,22 +28,25 @@ namespace HttpServer
             Console.WriteLine("Server is up.");
             Socket sock = serverSocket.AcceptSocket();
 
-            while (staticDynamic)
+            while (Static)
             {
-                TcpClient tcp = serverSocket.AcceptTcpClient();
-                Console.WriteLine("Client connected, lets go.");
-                Stream server = tcp.GetStream();
-                StreamReader sr = new StreamReader(server);
-                StreamWriter sw = new StreamWriter(server);
-                string request = sr.ReadLine();
-                sw.Write("Hey, the time of your request was: " +DateTime.Now);
-              
-                sw.AutoFlush = true;
-                tcp.Close();
+                
+               
+                    TcpClient tcp = serverSocket.AcceptTcpClient();
+                    Console.WriteLine("Client connected, lets go.");
+                    Stream server = tcp.GetStream();
+                    StreamReader sr = new StreamReader(server);
+                    StreamWriter sw = new StreamWriter(server);
+                    sw.Write("Hey, the time of your connection was: " + DateTime.Now);
+                    string request = sr.ReadLine();
+                    
+                    sw.AutoFlush = true;
+                    tcp.Close();
+                
 
             }
 
-           while (true)
+           while (Dynamic)
            {
                TcpClient tcp = serverSocket.AcceptTcpClient();
                Console.WriteLine("Dynamic client connected, lets go.");
@@ -48,16 +54,56 @@ namespace HttpServer
                StreamReader sr = new StreamReader(server);
                StreamWriter sw = new StreamWriter(server);
                string s = sr.ReadLine();
+             
                if (s != null)
                {
                    s = s.Split('/')[1];
                    s = s.Remove(s.Length - 5);
                }
                sw.Write("Hey, the file you have requested was: " + s + " at " + DateTime.Now);
-
+             
                sw.AutoFlush = true;
                tcp.Close();
            }
+
+
+                while (openFile)
+               {
+                   TcpClient tcp = serverSocket.AcceptTcpClient();
+                   Console.WriteLine("Dynamic client connected, lets go.");
+                   Stream server = tcp.GetStream();
+                   StreamReader sr = new StreamReader(server);
+                   StreamWriter sw = new StreamWriter(server);
+                   string s = sr.ReadLine();
+                   string[] file = s.Split(' ');
+                   string filename = file[1];
+                   string fullfile = RootCatalog + filename;
+
+                   try
+                   {
+                       FileStream stream = new FileStream(fullfile, FileMode.Open, FileAccess.Read);
+                       stream.CopyTo(sw.BaseStream);
+                       stream.Close();
+                   }
+                   catch (FileNotFoundException)
+                   {
+                       sw.Write("HTTP/1.0 404 Not Found\n\r");
+                       sw.Write("\r\n");
+                       sw.Flush();
+                   }
+                   catch (DirectoryNotFoundException)
+                   {
+                       sw.Write("HTTP/1.0 404 Not Found\n\r");
+                       sw.Write("\r\n");
+                       sw.Flush();
+                   }
+                   sw.AutoFlush = true;
+                   tcp.Close();
+               }
+           
+              
+               
+           
         }
      
     }
