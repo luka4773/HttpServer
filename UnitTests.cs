d8284c8fd569090dfeb7045a994c10250b8a6205
luka4773 <lukas.cech12@gmail.com>
2014-10-03 11:44:07 +0200

    Unit tests by our beloved teacher :D

---
+++ UnitTests.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+
+namespace UnitTestsByAnders
+{
+    [TestClass]
+   public class HttpServerTest
+    {
+        private static HttpServer.HttpServer server;
+        private const string CrLf = "\r\n";
+
+        [ClassInitialize]
+        public static void StartServer(TestContext context)
+        {
+           server = new HttpServer.HttpServer(8080);
+           Task.Factory.StartNew(server.RunServer);
+        }
+         
+
+        [TestMethod]
+        public void TestGet()
+        {
+            String line = GetFirstLine("GET /InstallDragonAgeOrigins.log HTTP/1.0");
+            Assert.AreEqual("HTTP/1.0 200 OK", line);
+
+            line = GetFirstLine("GET /fileDoesNotExist.txt HTTP/1.0");
+            Assert.AreEqual("HTTP/1.0 404 Not Found", line);
+        }
+
+
+        [TestMethod]
+        public void TestGetIllegalRequest()
+        {
+            String line = GetFirstLine("GET /file.txt HTTP 1.0");
+            Assert.AreEqual("HTTP/1.0 404 Not Found", line);
+        }
+
+        [TestMethod]
+        public void TestGetIllegalMethodName()
+        {
+            String line = GetFirstLine("PLET /file.txt HTTP/1.0");
+            Assert.AreEqual("HTTP/1.0 400 Illegal request", line);
+        }
+
+        [TestMethod]
+        public void TestGetIllegalProtocol()
+        {
+            String line = GetFirstLine("GET /file.txt HTTP/1.2");
+            Assert.AreEqual("HTTP/1.0 400 Illegal protocol", line);
+        }
+
+        [TestMethod]
+        public void TestMethodNotImplemented()
+        {
+            String line = GetFirstLine("POST /file.txt HTTP/1.0");
+            Assert.AreEqual("HTTP/1.0 200 xxx", line);
+        }
+
+        [ClassCleanup]
+        public static void StopServer()
+        {
+            server.Stop();
+        }
+        
+        /// <summary>
+        /// Private helper method
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
+        private static String GetFirstLine(String request)
+        {
+            
+            TcpClient client = new TcpClient("localhost", 8080);
+            NetworkStream networkStream = client.GetStream();
+
+            StreamWriter toServer = new StreamWriter(networkStream, Encoding.UTF8);
+            toServer.Write(request + CrLf);
+            toServer.Write(CrLf);
+            toServer.Flush();
+
+            StreamReader fromServer = new StreamReader(networkStream);
+            String firstline = fromServer.ReadLine();
+            toServer.Close();
+            fromServer.Close();
+            client.Close();
+            return firstline;
+
+        }
+    }
+}
+
